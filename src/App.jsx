aecd63e1231545785845a26325b0f36756d40070
MauroSan <maurocicero@gmail.com>
2024-06-06 14:29:33 +0000

    Estrutura HTML criada

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ function App() {
   return (
 
     <>
-
-      App
-
-    </>
+<div>
+  <h2>Rick Sanchez</h2> 
+  <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
+</div>
+        </>
 
   )
 }
