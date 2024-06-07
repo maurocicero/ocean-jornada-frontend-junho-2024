7aa87ce28f25e696e7059dd80ec36c43733975df
MauroSan <maurocicero@gmail.com>
2024-06-07 14:26:53 +0000

    2 cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,21 @@ import './App.css'
 import Card from './components/Card/Card'
 
 function App() {
+  const item1 = {
+    name: 'Rick Sanchez',
+    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  }
+
+  const item2 = {
+    name: 'Morty Smith',
+    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  }
+
   return (
     <>
       <div className="cards">
-        <Card />
-        <Card />
+        <Card item={item1} />
+        <Card item={item2} />
       </div>
     </>
   )
