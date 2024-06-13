21f106710e4b78d19da85cd3e3d28a97c9e2eb05
MauroSan <maurocicero@gmail.com>
2024-06-13 13:22:23 +0000

    Exibindo itens carregados via API

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Card from './components/Card/Card'
 
 function App() {
-  const item1 = {
-    name: 'Rick Sanchez',
-    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
-  }
+    
+  const [itens, setItens] = useState([])
 
-  const item2 = {
-    name: 'Morty Smith',
-    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
-  }
+  async function carregarDados () {
+  const apiUrl = 'https://rickandmortyapi.com/api/character'
 
-  const item3 = {
-    name: 'Summer Smith',
-    image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
-  }
-
-  // Listas (Arrays)
-  const itens = [item1, item2, item3]
+  const response = await fetch(apiUrl)
+  const body = await response.json()
+  const results = body.results
+  console.log(results)
+  console.log('results', results)
+  setItens (results)
+}
+useEffect(function(){
+  carregarDados()
+}, [])
 
   return (
     <>
